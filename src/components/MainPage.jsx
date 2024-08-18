fdacd415e0e3d5229a1a8ecfaecf3aad1d385491
Mojtaba Seddighi <mojtaba@work.chaleshsoft.com>
2024-08-18 14:21:48 +0330

    component edited and boreder is 2px

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -9,6 +9,8 @@ import play from '../assets/playstation_31.png';
 import Airpad from '../assets/airpad_24.png';
 import '../index.css'
 import tvroom from '../assets/tv_36.png'
+import { PiCheckCircleFill } from "react-icons/pi";
+import { GoArrowRight } from "react-icons/go";
 
 
 const FeaturedProducts = () => {
@@ -51,7 +53,7 @@ const FeaturedProducts = () => {
                                 <img src={play} alt="image_not_found"/>
                             </div>
                             <div className="item_content position-relative">
-                                <h4 className="">
+                                <h4 className="item_title">
                                     <a href="#!">
                                         پلی استیشن 4 برند سونی </a>
                                 </h4>
@@ -77,8 +79,8 @@ const FeaturedProducts = () => {
                     </div>
                 </div>
 
-                <a className="flex mt-20" href="#!">
-                    <span>VIEW MORE</span>
+                <a className="flex mt-20 view-more-link" href="#!">
+                    <span className={"flex items-center gap-2"}>VIEW MORE <GoArrowRight /></span>
                     {/*<BsArrowRight />*/}
                 </a>
             </div>
@@ -106,11 +108,58 @@ const FeaturedProducts = () => {
                                 <div className={'p-8 w-full bg-[#ECF7F3]'}>
                                     <img src={tvroom} alt="" style={{backgroundColor: "#ECF7F3"}}/>
                                 </div>
+                                <div className={"ml-96"}>
+                                    <h3> محصول ویژه هفته به همراه آفر </h3>
+                                </div>
+                                <div className=" ">
+                                    <ul className="">
+                                        <li className={"flex items-center"}><PiCheckCircleFill color={'green'}/> -میزان
+                                            کیفیت تصویر : 1300
+                                        </li>
+                                        <li className={"flex items-center"}><PiCheckCircleFill color={'green'}/>-وضوح
+                                            تصویر &amp; HDR
+                                        </li>
+                                        <li className={"flex items-center"}><PiCheckCircleFill color={'green'}/>-قابلیت
+                                            اتصال بلوتوث
+                                        </li>
+                                        <li className={"flex items-center"}><PiCheckCircleFill color={'green'}/> دارای 8
+                                            ورودی مختلف کابل
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className={"flex gap-2 ml-4"}>
+                                    <div className={"flex flex-col p-3"}>
+                                        <strong className={"bg-gray-200"}> 00 </strong>
+                                        <span> Days </span>
+                                    </div>
+                                    <div className={"flex flex-col p-3"}>
+                                        <strong className={"bg-gray-200"}> 00 </strong>
+                                        <span> Hourse </span>
+                                    </div>
+                                    <div className={"flex flex-col p-3"}>
+                                        <strong className={"bg-gray-200"}> 00 </strong>
+                                        <span> Mins </span>
+                                    </div>
+                                    <div className={"flex flex-col p-3"}>
+                                        <strong className={"bg-gray-200"}> 00 </strong>
+                                        <span> Secs </span>
+                                    </div>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide className={'flex flex-col gap-2'}>
+                                <div className={'p-8 w-full bg-[#ECF7F3]'}>
+                                <img src={tvroom} alt="" style={{backgroundColor: "#ECF7F3"}}/>
+                                </div>
                                 <div className="counter_wrap">
                                     <ul className="item_info_list ul_li_block clearfix">
-                                        <li>- Picture Quality Index: 1300</li>
-                                        <li>- Technologia PurColour &amp; HDR</li>
-                                        <li>- Android TV</li>
+                                        <li className={"flex items-center"}><PiCheckCircleFill color={'green'}/>-Picture
+                                            Quality Index: 1300
+                                        </li>
+                                        <li className={"flex items-center"}><PiCheckCircleFill color={'green'}/>-Technologia
+                                            PurColour &amp; HDR
+                                        </li>
+                                        <li className={"flex items-center"}><PiCheckCircleFill color={'green'}/>-Android TV
+                                        </li>
                                     </ul>
                                     <ul
                                         className="countdown_timer timein_box ul_li clearfix"
@@ -124,9 +173,77 @@ const FeaturedProducts = () => {
                                 </div>
                                 <div className="counter_wrap">
                                     <ul className="item_info_list ul_li_block clearfix">
-                                        <li>- Picture Quality Index: 1300</li>
-                                        <li>- Technologia PurColour &amp; HDR</li>
-                                        <li>- Android TV</li>
+                                        <li className={"flex items-center"}><PiCheckCircleFill color={'green'}/>-Picture
+                                            Quality Index: 1300
+                                        </li>
+                                        <li className={"flex items-center"}><PiCheckCircleFill color={'green'}/>-Technologia
+                                            PurColour &amp; HDR
+                                        </li>
+                                        <li className={"flex items-center"}><PiCheckCircleFill color={'green'}/>-Android
+                                            TV
+                                        </li>
+                                    </ul>
+                                    <ul
+                                        className="countdown_timer timein_box ul_li clearfix"
+                                        data-countdown="2021/3/24"
+                                    ></ul>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide className={'flex flex-col gap-2'}>
+                                <div className={'p-8 w-full bg-[#ECF7F3]'}>
+                                    <img src={tvroom} alt="" style={{backgroundColor: "#ECF7F3"}}/>
+                                </div>
+                                <div className="counter_wrap">
+                                    <ul className="item_info_list ul_li_block clearfix">
+                                        <li className={"flex items-center"}><PiCheckCircleFill color={'green'}/>-Picture
+                                            Quality Index: 1300
+                                        </li>
+                                        <li className={"flex items-center"}><PiCheckCircleFill color={'green'}/>-Technologia
+                                            PurColour &amp; HDR
+                                        </li>
+                                        <li className={"flex items-center"}><PiCheckCircleFill color={'green'}/>-Android
+                                            TV
+                                        </li>
+                                    </ul>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide className={'flex flex-col gap-2'}>
+                                <div className={'p-8 w-full bg-[#ECF7F3]'}>
+                                    <img src={tvroom} alt="" style={{backgroundColor: "#ECF7F3"}}/>
+                                </div>
+                                <div className="counter_wrap">
+                                    <ul className="item_info_list ul_li_block clearfix">
+                                        <li className={"flex items-center"}><PiCheckCircleFill color={'green'}/>-Picture
+                                            Quality Index: 1300
+                                        </li>
+                                        <li className={"flex items-center"}><PiCheckCircleFill color={'green'}/>-Technologia
+                                            PurColour &amp; HDR
+                                        </li>
+                                        <li className={"flex items-center"}><PiCheckCircleFill color={'green'}/>-Android
+                                            TV
+                                        </li>
+                                    </ul>
+                                    <ul
+                                        className="countdown_timer timein_box ul_li clearfix"
+                                        data-countdown="2021/3/24"
+                                    ></ul>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide className={'flex flex-col gap-2'}>
+                                <div className={'p-8 w-full bg-[#ECF7F3]'}>
+                                    <img src={tvroom} alt="" style={{backgroundColor: "#ECF7F3"}}/>
+                                </div>
+                                <div className="counter_wrap">
+                                    <ul className="item_info_list ul_li_block clearfix">
+                                        <li className={"flex items-center"}><PiCheckCircleFill color={'green'}/>-Picture
+                                            Quality Index: 1300
+                                        </li>
+                                        <li className={"flex items-center"}><PiCheckCircleFill color={'green'}/>-Technologia
+                                            PurColour &amp; HDR
+                                        </li>
+                                        <li className={"flex items-center"}><PiCheckCircleFill color={'green'}/>-Android
+                                            TV
+                                        </li>
                                     </ul>
                                     <ul
                                         className="countdown_timer timein_box ul_li clearfix"
@@ -191,12 +308,11 @@ const FeaturedProducts = () => {
                     </div>
                 </div>
 
-                <a className="flex mt-20" href="#!">
-                    <span>VIEW MORE</span>
-                    {/*<BsArrowRight />*/}
+                <a className="flex mt-20 view-more-link" href="#!">
+                    <span className={"flex items-center gap-2"}>VIEW MORE<GoArrowRight /></span>
                 </a>
             </div>
-            <div className="">
+            <div className="featured_products_wrap">
                 <h3 className="mb-4"> تنوع محصولات </h3>
                 <div className="flex items-center mb-4">
                     <div className="h-[3px] w-1/12 bg-red-500"></div>
@@ -232,9 +348,9 @@ const FeaturedProducts = () => {
                             <div className="">
                                 <img src={play} alt="image_not_found"/>
                             </div>
-                            <div className="item_content position-relative">
+                            <div className="item_title">
                                 <h4 className="">
-                                    <a href="#!">
+                                    <a href="#!" className={"item_title"}>
                                         پلی استیشن 40 برند سونی </a>
                                 </h4>
                                 <span className="item_price">10/000/000 ریال </span>
@@ -258,12 +374,12 @@ const FeaturedProducts = () => {
                         </div>
                     </div>
                 </div>
-
-                <a className="flex mt-20" href="#!">
-                    <span>VIEW MORE</span>
+                <a className="flex mt-20 view-more-link" href="#!">
+                    <span className={"flex items-center gap-2"}>VIEW MORE<GoArrowRight /></span>
                     {/*<BsArrowRight />*/}
                 </a>
             </div>
+
         </div>
     );
 };
